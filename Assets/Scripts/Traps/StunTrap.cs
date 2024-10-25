32f3169aa47163a85c39755fade30b88d026defb
Isdre <guszczagilbert@gmail.com>
2024-10-25 21:19:58 +0200

    teacher

---
+++ Assets/Scripts/Traps/StunTrap.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Traps {
+    public abstract class StunTrap : Trap
+    {
+        public int TTL = 1;
+
+        public virtual void OnChildEnter(GameObject target) {
+            TTL--;
+            Debug.Log("Child");
+            if (TTL == 0) Destroy(this.gameObject);
+        }
+
+        public virtual void OnKindergartenerEnter(GameObject target) {
+            TTL--;
+            Debug.Log("Kindergartener");
+            if (TTL == 0) Destroy(this.gameObject);
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D col) {
+            if (col.gameObject.CompareTag("Child")) OnChildEnter(col.gameObject);
+            if (col.gameObject.CompareTag("Kindergartener")) OnKindergartenerEnter(col.gameObject);
+        }
+    }
+}
