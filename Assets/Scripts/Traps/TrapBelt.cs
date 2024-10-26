8230054cc0a0a5ce5a335a1a62b2e6675a9bec49
Isdre <guszczagilbert@gmail.com>
2024-10-26 16:52:48 +0200

    Buy/Set Traps

--- Assets/Scripts/Traps/TrapBelt.cs
+++ Assets/Scripts/Traps/TrapBelt.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Linq;
+
 namespace Traps {
     [System.Serializable]
     public struct TrapPref {
-        public string name;
+        public string Name;
         public Sprite icon;
         public GameObject prefab;
     }
 
 public class TrapBelt : MonoBehaviour
 {
+    public static TrapBelt Instance = null;
     public List<TrapPref> trapDefs = new();
     public List<int> trapCount;
     public List<TrapSlot> slots = new();
@@ -20,6 +23,9 @@ public class TrapBelt : MonoBehaviour
     private int selectedTrap = 0;
 
     private void Awake() {
+        if (Instance == null) Instance = this;
+        if (Instance != this) Destroy(this.gameObject);
+
         foreach(TrapPref trapPref in trapDefs) {
             TrapSlot ts = Instantiate(trapSlot,transform).GetComponent<TrapSlot>();
             slots.Add(ts);
@@ -27,6 +33,12 @@ public class TrapBelt : MonoBehaviour
             ts.SetAmount(0);
             trapCount.Add(0);
         }
+    }
+
+    public void AddTrap(string n) {
+        int target = trapDefs.FindIndex(x => x.Name == n);
+        trapCount[target]++;
+        slots[target].SetAmount(trapCount[target]);
     }
 }
 }
--- Assets/Scripts/Traps/TrapSetter.cs
+++ Assets/Scripts/Traps/TrapSetter.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Traps {
+    public class TrapSetter : MonoBehaviour {
+        private TrapBelt belt;
+        [SerializeField] private int activeId;
+        private int beltWidth;
 
-    [System.Serializable]
-    public struct TrapCount {
-        public TrapCount(GameObject n, int c) {
-            trap = n;
-            count = c;
+        public void Start() {
+            belt = TrapBelt.Instance;
+            beltWidth = belt.trapDefs.Count;
+            belt.slots[activeId].IsSelected(true);
         }
 
-        public GameObject trap;
-        public int count;
-    }
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Q)) changeSelected(-1); 
+            if (Input.GetKeyDown(KeyCode.E)) changeSelected(1);
+            if (Input.GetKeyDown(KeyCode.R)) SetTrap();
+        }
 
-    public class TrapSetter : MonoBehaviour
-    {
-        [SerializeField] private int activeId;
+        private void SetTrap() {
+            if (belt.trapCount[activeId] <= 0) return;
+            belt.trapCount[activeId]--;
+            belt.slots[activeId].SetAmount(belt.trapCount[activeId]);
+            Instantiate(belt.trapDefs[activeId].prefab,transform.position, Quaternion.identity);
+        }
+
+        private void changeSelected(int change) {
+            belt.slots[activeId].IsSelected(false);
+            activeId += change;
+            if (activeId == beltWidth) activeId = 0;
+            if (activeId == -1) activeId = beltWidth - 1;
+            belt.slots[activeId].IsSelected(true);
+        }
     }
 }
