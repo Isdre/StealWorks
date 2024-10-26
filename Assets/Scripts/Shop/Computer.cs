62f3deb4a99af52330bee012dbb53364fa3144b5
Isdre <guszczagilbert@gmail.com>
2024-10-26 14:48:57 +0200

    make computer great again

--- Assets/Scripts/Shop/Computer.cs
+++ Assets/Scripts/Shop/Computer.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace Shop{
+    [System.Serializable]
+    public struct ItemDef {
+        public string name; 
+        public Sprite sprite;
+        public int gold;
+    }
+
     public class Computer : MonoBehaviour {
-        [SerializeField] GameObject icon;
-        [SerializeField] GameObject shop;
+        public List<ItemDef> itemsDef = new();
+        [SerializeField] private GameObject itemSlotPrefab;
+        [SerializeField] private Transform container;
+        [SerializeField] private GameObject icon;
+        [SerializeField] private GameObject shop;
         private bool _iconIs = false;
+
+        private void Start() {
+            foreach(ItemDef i in itemsDef) {
+                Item item = Instantiate(itemSlotPrefab,container).GetComponent<Item>();
+                item.gold = i.gold;
+                item.Name = i.name;
+                item.SetParam(i.sprite);
+            }
+        }
 
         private void Update() {
             if (_iconIs && Input.GetKeyDown(KeyCode.F)) SetShop(true); 
